d6099530dfbfc3f74409273de05cdc2141cafffa
Armash Ansari <172116296+armash66@users.noreply.github.com>
2026-01-24 03:34:07 +0530

    Progrss on ui still pending

--- eyegpt_web/db.py
+++ eyegpt_web/db.py
@@ -1,23 +1,28 @@
 import sqlite3
+import os
 
-DB_NAME = "history.db"
-
-def get_db():
-    return sqlite3.connect(DB_NAME)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "history.db")
 
 def init_db():
-    conn = sqlite3.connect("history.db")
+    conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
+
     c.execute("""
         CREATE TABLE IF NOT EXISTS history (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            image_path TEXT,
+            image_path TEXT NOT NULL,
             gradcam_path TEXT,
             prediction TEXT,
             confidence REAL,
             timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
         )
     """)
+
     conn.commit()
     conn.close()
 
+def get_db():
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    return conn
